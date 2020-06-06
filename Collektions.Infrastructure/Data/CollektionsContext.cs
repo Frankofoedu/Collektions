6b5e74f8b01da6da89fb786a0b1c76cf2521d0ca
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-06-06 16:28:55 +0100

    added ef context and repo

---
+++ Collektions.Infrastructure/Data/CollektionsContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Collektions.Core.Entities;
+
+namespace Collektions.Infrastructure.Data
+{
+    class CollektionsContext : DbContext
+    {
+        public CollektionsContext(DbContextOptions<CollektionsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<House> Houses { get; set; }
+    }
+}
---
+++ Collektions.Infrastructure/EfRepository.cs
@@ -0,0 +1,57 @@
+﻿using Collektions.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Collektions.Core.Entities;
+using System.Threading.Tasks;
+
+namespace Collektions.Infrastructure
+{
+    public class EfRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    {
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstAsync(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstOrDefaultAsync(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
