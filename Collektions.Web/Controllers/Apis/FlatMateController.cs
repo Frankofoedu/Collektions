769150367cc298190ae84b7c6f8ca9974916f08d
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-06-06 16:29:44 +0100

    added api project

---
+++ Collektions.Web/Controllers/Apis/FlatMateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Collektions.Core.Entities;
+using Collektions.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Collektions.Web.Controllers.Apis
+{
+    public class FlatMateController : Controller
+    {
+        readonly IAsyncRepository<FlatMate> _flatMatesRepository;
+        public FlatMateController(IAsyncRepository<FlatMate> flatMatesRepository)
+        {
+            _flatMatesRepository = flatMatesRepository;
+        }
+
+        public Task<FlatMateViewModel> GetFlatMateById(int id, int houseId)
+        {
+            _flatMatesRepository.FirstAsync()
+        }
+
+    }
+}
---
+++ Collektions.Web/Controllers/Apis/HouseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Collektions.Core.Entities;
+using Collektions.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Collektions.Web.Controllers.Apis
+{
+    public class HouseController : BaseApiController
+    {
+        readonly IAsyncRepository<House> _houseRepository;
+        public HouseController(IAsyncRepository<House> houseRepository)
+        {
+            _houseRepository = houseRepository;
+        }
+        [HttpGet("GetHouseById")]
+        public async Task<House> GetAsync(int id)
+        {
+            return await _houseRepository.GetByIdAsync(id);
+        }
+    }
+}
