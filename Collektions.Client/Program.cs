f1d8fc52626711ee2a2b53f0017f59146aa9f3e2
Okpara Obinna <obainoforgod@gmail.com>
2020-06-28 23:26:16 +0100

    Added some pages and components

--- Collektions.Client/Program.cs
+++ Collektions.Client/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.Modal;
+using Blazored.Toast;
 
 namespace Collektions.Client
 {
@@ -27,6 +29,9 @@ namespace Collektions.Client
             builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Collektions.ServerAPI"));
 
             builder.Services.AddApiAuthorization();
+
+            builder.Services.AddBlazoredModal();
+            builder.Services.AddBlazoredToast();
 
             await builder.Build().RunAsync();
         }
